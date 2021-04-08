c7730cc34ec6c374623cd4d2c673082c194c8894
mo <fire.senfony@gmail.com>
2021-04-07 23:07:30 -0400

    worked on serial

--- src/motor_control/src/serial_talker.py
+++ src/motor_control/src/serial_talker.py
@@ -4,16 +4,25 @@ from time import sleep
 COM = '/dev/ttyUSB0'
 BAUD = 115200
 
-ser = serial.Serial(COM, BAUD, timeout = .1)
+ser = serial.Serial(COM, BAUD, timeout=.1)
 
-print('Waiting for device');
+print('Waiting for device')
 sleep(1)
 print(ser.name)
 
 
 while True:
-	try:
-		val = str(ser.readline().decode().strip('\r\n'))
-		print(val, end="\n")
-	except(UnicodeDecodeError):
-		pass
+    try:
+        ser.write(b"blue\n")
+        val = str(ser.readline().decode().strip('\r\n'))
+        val = val.split()
+        if (len(val) == 5):
+            # distance in micrometers, velocity in micrometer/milisec
+            distance_right_wheel = val[1]
+            distance_left_wheel = val[2]
+            vel_right = val[3]
+            vel_left = val[4]
+        # print(dx, end="\n")
+        print(val, end="\n")
+    except(UnicodeDecodeError):
+        pass
