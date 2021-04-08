6e74da0f9a490c8c768a0d3a0606b02130cacbd3
mo <fire.senfony@gmail.com>
2021-04-07 22:26:51 -0400

    enabled timer interupt along with pwm feedback

--- arduino/motorController.ino
+++ arduino/motorController.ino
@@ -7,8 +7,8 @@
    manual spinning of the wheel since there's only one hall sensor working.
 
    No warranties or guarantees whatsoever, use at your own risk. tip, make sure you have short circuit protection at your power source.
-
 */
+#include <Ticker.h>
 
 // constants
 
@@ -34,8 +34,8 @@ const int in4 = 4;
 const int rencoder = 0;
 
 //  odometry values
-long int lencoder_pos = 0;
-long int rencoder_pos = 0;
+volatile long int lencoder_pos = 0;
+volatile long int rencoder_pos = 0;
 
 // these 4 flags help us determing odomotry direction with just one hall sensor instead of two
 // when all of them are false then both motors are stopped.
@@ -45,94 +45,55 @@ bool is_right_forward = false;
 bool is_left_reverse = false;
 bool is_right_reverse = false;
 
+// controller params
+String cmd;
+const int  gear_ratio = 21;
+const int ticks_rev =  11; // encoder ticks per wheel rev
+const int pulses_per_rev = gear_ratio * ticks_rev;
+const long wheel_diameter = 65000; // wheel diameter in micrometers, so 6.5 cm
+const long radius = wheel_diameter / 2;
+const long distance_per_rev = 204000; //20.4 cm convered per wheel reveloution put in micrometers
+const long distance_per_pulse = distance_per_rev / pulses_per_rev; // every encoder tick we conver this distance
+// const long timer_dt = 2500000; // this translates to 0.5sec aborted tmr1 interferes with pwm generation
+//const double dt = 0.5; // for easier calc, dont change this alone, u have to set the timer val above first
+volatile unsigned long previous_time = 0;
+volatile int ledState = LOW;
+volatile unsigned long next;
+Ticker timer;
+const int interval = 200; // 200ms or 0.2sec
+
 // optional parameters
 const int turn_delay = 0;
 const int spin_direction_change_delay = 0;
-const int debug_delay = 1000;
-const bool enable_debug_msgs = true;
-const bool enable_debug_odometer_msgs = true;
+const int debug_delay = 100;
+const bool enable_debug_msgs = false;
+const bool enable_debug_odometer_msgs = false;
 bool unknown_right_spin_dir = false;
 bool unknown_left_spin_dir = false;
+const long approx_time = 8388606; // 80MHZ assuming prescaler so 80cpu beats per 1us, so that num will give us 0.1sec approx if prescaler assumption is good.
+const long offset = 9500;
 
-// controller communication params
-String cmd;
-const long velocity_send_interval = 100; //milisec, send the wheels feedback to the PI every time this elapses
-unsigned long previousMillis_vel = 0;
 
-//flashing led
-int ledState = LOW;
-unsigned long previousMillis = 0;
-const long interval = 1000;
-
-void setup() {
-  Serial.begin(115200);
-  // Set all the motor control pins to outputs
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
-  pinMode(lencoder, INPUT);
-  pinMode(rencoder, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  // Turn off motors, Initial state
-  turn_off_motors_force();
-  //direction_test();
-
-  attachInterrupt(digitalPinToInterrupt(lencoder), update_lencoder, RISING);
-  attachInterrupt(digitalPinToInterrupt(rencoder), update_rencoder, RISING);
+/* interrupts ISR */
+void ICACHE_RAM_ATTR send_feedback_to_PID() {
+  /* sends encoder feedback to the dragonboard, which sends it to the PID ros node*/
+  if (enable_debug_msgs){Serial.println("In the ISR baby");}
+  unsigned long current_time = millis();
+  Serial.println(current_time);
+  if (current_time - previous_time >= interval) {
+    
+    long dt = current_time - previous_time;
+    led_control();
+    send_feedback(dt);
+    previous_time = current_time;
+    if (enable_debug_msgs){Serial.print("delta t us:");Serial.println(dt);}
+  }
+  // arm the next interrupt
+  next = ESP.getCycleCount() + approx_time;
+  timer0_write(next+offset);
 }
-
-void loop() {
-  led_control();
-  //test_motor_drive();
-  String myString = "7300,2250\n";
-  myString.trim();
-int commaIndex = myString.indexOf(',');
-int vr = myString.substring(0, commaIndex).toInt();
-int vl = myString.substring(commaIndex + 1).toInt();
-vr = map(vr, 0, 10000, MINPWM, MAXPWM);
-vl = map(vl, 0, 10000, MINPWM, MAXPWM);
-Serial.println(vr);
-Serial.println(vl);
-}
-
-void receive_velocity_cmd(){
-  if (Serial.available()) {
-    cmd = Serial.readStringUntil('\n');
-    cmd.trim();
-    int commaIndex = cmd.indexOf(',');
-    int vr = cmd.substring(0, commaIndex).toInt();
-int vl = cmd.substring(commaIndex + 1).toInt();
-Serial.println(vr);
-Serial.println(vl);
-    }
-  }
-void reset_encoder(){
-  lencoder_pos = 0;
-  rencoder_pos = 0;
-  }
-
-void send_encoder_ticks(){
-   Serial.print(lencoder_pos); Serial.print("\t");
-   Serial.print(rencoder_pos); Serial.print("\n");
-  }
-
-void led_control(){
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    if (ledState == LOW) {
-      ledState = HIGH;  // Note that this switches the LED *off*
-    } else {
-      ledState = LOW;  // Note that this switches the LED *on*
-    }
-    digitalWrite(LED_BUILTIN, ledState);
-    }
-  }
 
 ICACHE_RAM_ATTR void update_lencoder() {
-
   if (is_left_motor_going_forward()) {
     lencoder_pos++ ;
     unknown_left_spin_dir = false;
@@ -153,13 +114,10 @@ ICACHE_RAM_ATTR void update_lencoder() {
       Serial.println("Warning only 1 hall sensor can't determine spin direction on manuel spin, plz use software commands to spin the wheels");
     }
   }
-
-
 }
 
+
 ICACHE_RAM_ATTR void update_rencoder() {
-
-
   if (is_right_motor_going_forward()) {
     rencoder_pos++ ;
     unknown_right_spin_dir = false;
@@ -178,11 +136,116 @@ ICACHE_RAM_ATTR void update_rencoder() {
     if (unknown_right_spin_dir) {
       Serial.println("Warning only 1 hall sensor can't determine spin direction on manuel spin, plz use software commands to spin the wheels");
     }
-
   }
+}
 
+void setup() {
+  Serial.begin(115200);
+  // Set all the motor control pins to outputs
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(lencoder, INPUT);
+  pinMode(rencoder, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Turn off motors, Initial state
+  turn_off_motors_force();
+  //direction_test();
+
+  attachInterrupt(digitalPinToInterrupt(lencoder), update_lencoder, RISING);
+  attachInterrupt(digitalPinToInterrupt(rencoder), update_rencoder, RISING);
+
+  // aborted using timer1 as it interferes with PWM genration will use timer0
+  // timer1 stuff, set it to 0.5s https://www.visualmicro.com/page/Timer-Interrupts-Explained.aspx
+  //  timer1_attachInterrupt(send_feedback_to_PID); // Add ISR Function
+  //  timer1_enable(TIM_DIV16, TIM_EDGE, TIM_LOOP);
+  //  timer1_write(timer_dt);
+
+  // timer 0 stuff example from here https://hackaday.io/project/12818-not-grbl/log/47580-esp8266-timer0-and-isr
+  // btw timer0 stuff is hacky, it is an internal timer that is not ment to be configed, that's why i gotta count clock cycles, i think at least.
+  
+  noInterrupts();
+  timer0_isr_init();
+  timer0_attachInterrupt(send_feedback_to_PID);
+  next = ESP.getCycleCount() + approx_time;
+  timer0_write(next+offset);
+  interrupts();
+  
 
 }
+
+void loop() {
+  printf("in the loop \n");
+  Serial.println("HEYYYY ");
+//  /set_right_pwm(MAXPWM);
+  //led_control();
+  test_motor_drive();// enocder feedback interrupt doesnt work when pwm is going cuz they use the same timer
+  //send_feedback();
+  /*
+    String myString = "7300,2250\n";
+    myString.trim();
+    int commaIndex = myString.indexOf(',');
+    int vr = myString.substring(0, commaIndex).toInt();
+    int vl = myString.substring(commaIndex + 1).toInt();
+    vr = map(vr, 0, 10000, MINPWM, MAXPWM);
+    vl = map(vl, 0, 10000, MINPWM, MAXPWM);
+    Serial.println(vr);
+    Serial.println(vl);
+  */
+}
+
+void receive_velocity_cmd() {
+  // takes velocity command from db and updates pwm for motors
+  if (Serial.available()) {
+    cmd = Serial.readStringUntil('\n');
+    cmd.trim();
+    int commaIndex = cmd.indexOf(',');
+    int vr = cmd.substring(0, commaIndex).toInt();
+    int vl = cmd.substring(commaIndex + 1).toInt();
+    Serial.println(vr);
+    Serial.println(vl);
+  }
+}
+
+void send_feedback(long dt) {
+  // sends feedback to the dragonboard with serial
+  long distance_right_wheel = rencoder_pos * distance_per_pulse;
+  long distance_left_wheel = lencoder_pos * distance_per_pulse;
+  long vel_right = distance_right_wheel / dt;
+  long vel_left = distance_left_wheel / dt;
+
+  Serial.print("wheels covered distance in micrometers, right then left: \t");
+  Serial.print(distance_right_wheel); Serial.print("\t");
+  Serial.print(distance_left_wheel); Serial.print("\n");
+
+  Serial.print("wheels vel micrometer/mili_sec, right then left: \t");
+  Serial.print(vel_right); Serial.print("\t");
+  Serial.print(vel_left); Serial.print("\n");
+  reset_encoder();
+}
+
+void reset_encoder() {
+  lencoder_pos = 0;
+  rencoder_pos = 0;
+}
+
+void send_encoder_ticks() {
+  Serial.print(lencoder_pos); Serial.print("\t");
+  Serial.print(rencoder_pos); Serial.print("\n");
+}
+
+void led_control() {
+  if (ledState == LOW) {
+    ledState = HIGH;
+  } else {
+    ledState = LOW;
+  }
+  digitalWrite(LED_BUILTIN, ledState);
+}
+
+
 
 void test_motor_drive() {
   // testing crtieria: must perform all tests without shortcircuiting the source, it is recommended that you
@@ -335,6 +398,7 @@ void turn_test() {
   }
   delay(debug_delay * 2);
 }
+
 
 ///// Motor control functions
 
